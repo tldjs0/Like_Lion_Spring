8203c64a74445c209c7fdc16fc4f2fc38c790b2e
sieon-park <154450816+tldjs0@users.noreply.github.com>
2026-06-30 10:25:17 +0900

    jpa_0629

--- src/main/java/com/example/demo/DTO/ArticleDTO.java
+++ src/main/java/com/example/demo/DTO/ArticleDTO.java
@@ -2,11 +2,12 @@ package com.example.demo.DTO;
 
 import com.example.demo.domain.Article;
 import lombok.Data;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Data
 public class ArticleDTO{
+    @Data
     public static class ArticleRes {
         private String title;
         private String content;
